1ea3f4e704066c15f22bb90192bf34b6c2dd835f
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2019-12-05 00:51:39 +0100

    131

--- src/sk/itsovy/Student.java
+++ src/sk/itsovy/Student.java
@@ -4,6 +4,7 @@ public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
 
 
     public Student() {
@@ -25,4 +26,11 @@ public class Student {
         this.lastName = lastName;
     }
 
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
